0631a691d3d257b5dcfec75ba55fdaeabc89422c
Ulu-pro <web.ulu7@gmail.com>
2023-08-02 20:30:43 +0500

    Some new constants and a new class

--- config.py
+++ config.py
@@ -1,2 +1,16 @@
 class Config:
     URL = 'https://telegra.ph/{title}-{month}-{day}'
+    ENTER_TITLE = 'Enter title: '
+
+    START_DATE = (2000, 1, 1)
+    END_DATE = (2000, 12, 31)
+
+
+class Colors:
+    OK = '\033[92m'
+    FAIL = '\033[91m'
+    RESET = '\033[0m'
+
+    @classmethod
+    def get_color(cls, status) -> str:
+        return cls.OK if status else cls.FAIL
