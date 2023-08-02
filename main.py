ed424ee232524dc4a92c99eacd24c286c2373eda
Ulu-pro <web.ulu7@gmail.com>
2023-08-03 00:37:50 +0500

    Completed functions and running in a loop

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
+import re
 from datetime import datetime, timedelta
-from typing import Generator, Union
+from typing import Generator
 
 import requests
 from bs4 import BeautifulSoup as Bs
+from requests.exceptions import ConnectionError
 from transliterate import translit
 from transliterate.exceptions import LanguageDetectionError
 
@@ -31,34 +34,69 @@ def date_generator() -> Generator[datetime, None, None]:
         current_date += timedelta(days=1)
 
 
-def get_next_url(url: str) -> str:
-    pass
+def get_next_url(url: str, end: str) -> str:
+    part = url.rsplit(end, maxsplit=1)[1][1:]
+    if part.isnumeric():
+        return re.sub(f'{part}$', str(int(part) + 1), url)
+
+    return f'{url}-{Config.SUFFIX}'
 
 
-def get_image_urls(content: bytes) -> list[str]:
-    pass
+def get_image_sources(content: bytes) -> list[str]:
+    soup = Bs(content, 'html.parser')
+    return [image['src'] for image in soup.find_all('img')]
+
+
+def download_image(source: str, prefix: str):
+    response = requests.get(Config.BASE_URL + source)
+    filename = prefix + '-' + source.split('/')[-1]
+
+    if not os.path.exists(Config.DOWNLOADS):
+        os.mkdir(Config.DOWNLOADS)
+
+    save_path = os.path.join(Config.DOWNLOADS, filename)
+    with open(save_path, 'wb') as file:
+        file.write(response.content)
 
 
 def display_info(url: str, ok: bool, images_count: int):
-    pass
+    print(Colors.get_color(ok) + str(images_count), url + Colors.RESET)
 
 
 def main():
     title = get_title()
 
     for date in date_generator():
-        url = Config.URL.format(
-            title=title,
+        date_structure = Config.DATE_STRUCTURE.format(
             month=date.strftime('%m'),
             day=date.strftime('%d')
         )
 
-        # response = requests.get(url)
-        # if response.ok:
-        #     image_urls = get_image_urls(response.content)
+        url = Config.URL.format(
+            title=title,
+            date=date_structure
+        )
 
-        print(url)
+        while True:
+            response = requests.get(url)
+            image_sources = get_image_sources(response.content)
+            prefix = url.rsplit('/', maxsplit=1)[1]
+            display_info(url, response.ok, len(image_sources))
+
+            if response.ok:
+                if len(image_sources) > 0:
+                    for image_source in image_sources:
+                        try:
+                            download_image(image_source, prefix)
+                        except ConnectionError:
+                            pass
+                url = get_next_url(url, date_structure)
+            else:
+                break
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Bye!')
